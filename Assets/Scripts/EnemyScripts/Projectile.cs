79cdc752b46116c254ebaf728dec30d5ed67d186
seanmul10 <36199832+seanmul10@users.noreply.github.com>
2020-03-11 19:29:02 +0000

    Ranged Enemy Initial Implementation
    Stopped working on this a while ago just got around to finishing it up, ranged enemy fires a Projectile. Both prefabs are set up with estimated values and will need tweaking

---
+++ Assets/Scripts/EnemyScripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    [HideInInspector]
+    public Vector3 direction;
+
+    private void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerMovement>())
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().TakeDamage(damage, "None");
+            Destroy(gameObject);
+        }
+    }
+}
