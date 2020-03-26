402da2b9c5694baddc9329f382d97decd0921daf
James O'Callaghan <psybergamer@gmail.com>
2020-03-26 00:53:31 +0000

    boss Pushed
    spawn at boss spawn point need the spells doing damage to him though,

--- Assets/Scripts/Entities/Enemies/Boss.cs
+++ Assets/Scripts/Entities/Enemies/Boss.cs
@@ -10,52 +10,75 @@ public class Boss : Enemy
     private int shot;
     protected override void Start()
     {
+        base.Start();
         startingHealth = 300;
         health = startingHealth;
         dead = false;
         shot = 0;
+        timeTillNextAttack = Time.time + timeBetweenAttack;
     }
     private void Update()
     {
         //fight starts
-        
-        if (!froze)
-        {
-            //health is > 33%
-            if (health > health / 100 * 33)
+        float distance = Vector3.Distance(player.position, transform.position); // Calculates the distance between the player and enemy
+
+        //health is > 33%
+        if (health > 300 / 100 * 33)
             {
-                float distance = Vector3.Distance(player.position, transform.position); // Calculates the distance between the player and enemy
+                
                 if (!froze)
+                {
+                if(attacking == false)
                 {
                     //keep distance to player 
                     // If the enemy is out of shooting range, move towards the player
                     if (distance > shootRange)
                     {
+                        
                         SB("persue");
                     }
                     // If the enemy gets within range of the player, shoot at them
                     else if (distance > disToPlayer)
                     {
+                        agent.enabled = true;
                         //dashes in to melee attack flurry of 4 strikes
                         MeleeAttack();
+                        if (Time.time >= timeTillNextAttack)
+                        {
+                            StartCoroutine(base.MeleeAttack());
+                        }
                         MeleeAttack();
+                        if (Time.time >= timeTillNextAttack)
+                        {
+                            StartCoroutine(base.MeleeAttack());
+                        }
                         MeleeAttack();
+                        if (Time.time >= timeTillNextAttack)
+                        {
+                            StartCoroutine(base.MeleeAttack());
+                        }
                         MeleeAttack();
+                        if (Time.time >= timeTillNextAttack)
+                        {
+                            StartCoroutine(base.MeleeAttack());
+                        }
                         //froze in place take double damage.
                         SB("freeze");
 
                         SB("flee"); // Effectively stops the enemy from moving
                     }
+                }
+                   
 
 
 
                     
                 }
             }
-            else if(health <= health/100*33)
+            else if(health <= 300/100*33)
             {
                 //health is < 33%
-                float distance = Vector3.Distance(player.position, transform.position); // Calculates the distance between the player and enemy
+                
                 //keep distance to player 
                 // If the enemy is out of shooting range, move towards the player
                 if (distance > shootRange)
@@ -80,7 +103,7 @@ public class Boss : Enemy
                 
                 
             }
-        }
+        
         
         
 
