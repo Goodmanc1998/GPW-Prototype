c28d5c791bc8b99c2c5a34eb3365caf10bcb9fd6
jackhardie <51489103+jackhardie@users.noreply.github.com>
2020-04-01 08:00:34 +0100

    door animation, prefab update, build probeles fixed

---
+++ Assets/Scripts/DoorAnimation.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DoorAnimation : MonoBehaviour
+{
+
+    public static string playerTag = "Player";
+
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            animator = GetComponent<Animator>();
+            animator.SetBool("Dooropen", false);
+
+    }
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == playerTag)
+        {
+            animator.SetBool("Dooropen", true);
+
+        }
+    }
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.tag == playerTag)
+        {
+            animator.SetBool("Dooropen", false);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
