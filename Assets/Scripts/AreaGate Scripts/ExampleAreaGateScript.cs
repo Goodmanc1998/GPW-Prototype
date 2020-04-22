69bb6b2280d5120051f244bad6a8b2fc386fa3c9
seanmul10 <36199832+seanmul10@users.noreply.github.com>
2020-04-22 21:35:04 +0100

    Added example AreaGate script
    Temporary, just to give an idea of how to use it

---
+++ Assets/Scripts/AreaGate Scripts/ExampleAreaGateScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Write multiple scripts for each group of waves that need to be detected when finished
+
+public class ExampleAreaGateScript : AreaGate
+{
+    // Use override keyword
+    public override void OpenArea()
+    {
+        // Stick all code
+        // and methods that
+        // need to be called
+        // after a group of
+        // waves is finished
+        // in here.
+    }
+}
