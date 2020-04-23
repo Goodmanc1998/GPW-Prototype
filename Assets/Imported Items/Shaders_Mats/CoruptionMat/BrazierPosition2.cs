ab27150bc6856c9d7a7d006b3c6d8c4403ca6157
jackhardie <51489103+jackhardie@users.noreply.github.com>
2020-04-23 16:25:15 +0100

    Wave script

--- Assets/Imported Items/Shaders_Mats/CoruptionMat/BrazierPosition2.cs
+++ Assets/Imported Items/Shaders_Mats/CoruptionMat/BrazierPosition2.cs
@@ -10,5 +10,9 @@ public class BrazierPosition2 : MonoBehaviour
     {
         // sends the transform position to the Coruption shader using Global vector
         Shader.SetGlobalVector("_Brazier2Position", transform.position);
+        void scaleTheRadius()
+        {
+            Shader.SetGlobalVector("_Braizer2Radius", new Vector4(100,0,0,0));
+        }
     }
 }
